96de0513a6ded58a4da0b790b661612cfebf7f91
kuba <kuba@macbook-air-kuba.home>
2023-08-30 16:36:28 +0200

    filters and sorting

--- components/load-more.js
+++ components/load-more.js
@@ -1,36 +1,36 @@
-"use client"
+// "use client"
 
-import { useEffect, useRef, useState } from "react"
-import { useIsVisible } from "../hooks/useIsVisible"
-import { searchProducts } from "../actions"
-import { useSearchParams } from "next/navigation"
+// import { useEffect, useRef, useState } from "react"
+// import { useIsVisible } from "../hooks/useIsVisible"
+// import { searchProducts } from "../actions"
+// import { useSearchParams } from "next/navigation"
 
-export default function LoadMore({ searchQuery, nextCursor, category, sortOption, lastPrice }) {
-    const [data, setData] = useState({
-        nextCursor: nextCursor, products: [], lastPrice: lastPrice
-    })
+// export default function LoadMore({ searchQuery, nextCursor, category, sortOption, lastPrice }) {
+//     const [data, setData] = useState({
+//         nextCursor: nextCursor, products: [], lastPrice: lastPrice
+//     })
 
-    const container = useRef(null)
-    const visible = useIsVisible(container)
+//     const container = useRef(null)
+//     const visible = useIsVisible(container)
 
-    useEffect(() => {
-        if (visible) {
-            searchProducts({ searchQuery, cursor: data.nextCursor, category: category, lastPrice: data.lastPrice, sortOption: sortOption }).then((res) => {
-                setData((data) => ({
-                    nextCursor: res.nextCursor,
-                    lastPrice: res.lastPrice,
-                    products: [
-                        ...data.products,
-                        ...res.products
-                    ]
-                }))
-            })
-        }
-    }, [visible])
+//     useEffect(() => {
+//         if (visible) {
+//             searchProducts({ searchQuery, cursor: data.nextCursor, category: category, lastPrice: data.lastPrice, sortOption: sortOption }).then((res) => {
+//                 setData((data) => ({
+//                     nextCursor: res.nextCursor,
+//                     lastPrice: res.lastPrice,
+//                     products: [
+//                         ...data.products,
+//                         ...res.products
+//                     ]
+//                 }))
+//             })
+//         }
+//     }, [visible])
 
-    return <>
-        {data.products}
-        {data.nextCursor ? <div ref={container}>REF</div> : <div>cipa</div>}
-        <span>xd</span>
-    </>
-}
+//     return <>
+//         {data.products}
+//         {data.nextCursor ? <div ref={container}>REF</div> : <div>cipa</div>}
+//         <span>xd</span>
+//     </>
+// }
