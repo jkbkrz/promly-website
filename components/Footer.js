16b9d9256440dd2d585200398919ceaed543882c
kuba <kuba@macbook-air-kuba.home>
2023-09-03 01:18:33 +0200

    footer layout

--- components/Footer.js
+++ components/Footer.js
@@ -2,6 +2,8 @@ import Link from "next/link"
 
 export default function Footer() {
     <Link href='#'>Polityka prywantości</Link>
-    return <span className="text-neutral-400 block text-sm text-center m-6">Created and developed by <a href="http://krauza.tech" target="_blank" className="text-blue-600 hover:underline font-mono">krauza.tech</a><br /><Link href='/privacy-policy' className="text-xs mr-2">Polityka Prywatności</Link><Link href='/contact' className="text-xs">Kontakt</Link><br /> © 2021 - 2023 Promly - All Rights Reserved.
+    return <span className="text-neutral-400 block text-sm text-center m-6">
+        <Link href='/privacy-policy' className="text-xs mr-2">Polityka Prywatności</Link><Link href='/contact' className="text-xs">Kontakt</Link><br />
+        Created and developed by <a href="http://krauza.tech" target="_blank" className="text-blue-600 hover:underline font-mono">krauza.tech</a><br /> © 2021 - 2023 Promly - All Rights Reserved.
     </span>
 }
