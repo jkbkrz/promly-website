250c700e23a287d5220f6166b1f1b864ac232b89
kuba <jakubkrauza411@gmail.com>
2024-01-03 17:23:41 +0100

    gtag test

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -26,10 +26,7 @@ export default function GoogleAnalytics({ GA_MEASUREMENT_ID }) {
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
 
-                gtag('consent', 'default', {
-                    'analytics_storage': 'denied'
-                });
-                
+              
                 gtag('config', '${GA_MEASUREMENT_ID}', {
                     page_path: window.location.pathname,
                 });
